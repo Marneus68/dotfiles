edcdfc834ba9a60853408c3ff7257e7d19838d73
Duane Bekaert <duane.bekaert@gmail.com>
2014-10-08 14:29:49 +0200

    Added installation script.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git submodule init
+git submodule update
+mv .bashrc ../
+mv .i3 ../
+mv .i3status.conf ../
+mv .vim ../
+mv .vimrc ../
+mv .zsh_aliases ../
+mv .zshenv ../
+mv .zshrc ../
+mv Templates ../
+
+
