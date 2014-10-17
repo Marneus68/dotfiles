c0454f327bb95cc34da5fd0f741c42f2db24060d
Duane Bekaert <duane.bekaert@gmail.com>
2014-10-17 10:34:39 +0200

    Modified the install script to use a destination path relative to the currently logged in user instead of a path relative to the directory you clonned the repo in.

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 git submodule init
 git submodule update
-mv .bashrc ../
-mv .i3 ../
-mv .i3status.conf ../
-mv .vim ../
-mv .vimrc ../
-mv .zsh_aliases ../
-mv .zshenv ../
-mv .zshrc ../
-mv Templates ../
-
+mv .bashrc ~/
+mv .i3 ~/
+mv .i3status.conf ~/
+mv .vim ~/
+mv .vimrc ~/
+mv .zsh_aliases ~/
+mv .zshenv ~/
+mv .zshrc ~/
+mv Templates ~/
 
